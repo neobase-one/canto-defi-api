f514aedb3fd64dbf89dcf85e1db93aeff7de054d
Siddarth Vijay <vsiddarthvijay@gmail.com>
2022-08-08 18:15:44 +0530

    transactions refactor

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -9,39 +9,32 @@ import { Swap } from "./swap";
 import { ObjectIdScalar } from "../types/objectIdScalar";
 import { ZERO_BD } from "../utils/constants";
 
-@ObjectType()
-export class Transaction {
-  @Field((type) => ObjectIdScalar)
-  @Property({ default: "", required: false })
+// mongo database object
+export class TransactionDb {
   readonly _id: ObjectId;
 
-  @Field((type) => ID)
+  // decorator docs: https://typegoose.github.io/typegoose/docs/api/decorators/prop
   @Property({ default: "", required: false })
   id: string;
 
-  @Field((type) => DecimalScalar)
   @Property({ default: new Decimal("0"), required: false })
   timestamp: Decimal;
 
-  @Field((type) => DecimalScalar)
   @Property({ default: new Decimal("0"), required: false })
   blockNumber: Decimal;
 
-  @Field((type) => [Mint])
   @Property({ default: [], required: false, type: () => Mint })
   mints: string[]; // todo: how to return Mint object
 
-  @Field((type) => [Burn])
   @Property({ default: [], required: false, type: () => Burn })
   burns: string[];
 
-  @Field((type) => [Swap])
   @Property({ default: [], required: false, type: () => Swap })
   swaps: string[];
 
-  constructor (hash: string) {
+  constructor(id: string) {
     this._id = new ObjectId();
-    this.id = hash;
+    this.id = id;
     this.timestamp = ZERO_BD;
     this.blockNumber = ZERO_BD;
     this.mints = [];
@@ -50,4 +43,53 @@ export class Transaction {
   }
 }
 
-export const TransactionModel = getModelForClass(Transaction);
+export const BlockModel = getModelForClass(TransactionDb);
+
+// graphql return object (type Block as shown in schema.ts)
+// decorator docs: https://typegraphql.com/docs/types-and-fields.html 
+@ObjectType()
+export class Transaction {
+  @Field((type) => ObjectIdScalar)
+  readonly _id: ObjectId;
+
+  @Field((type) => ID)
+  id: string;
+
+  @Field((type) => DecimalScalar)
+  timestamp: Decimal;
+
+  @Field((type) => DecimalScalar)
+  blockNumber: Decimal;
+
+  @Field((type) => [Mint])
+  mints: string[]; // todo: how to return Mint object
+
+  @Field((type) => [Burn])
+  burns: string[];
+
+  @Field((type) => [Swap])
+  swaps: string[];
+
+  constructor(hash: string) {
+    this._id = new ObjectId();
+    this.id = hash;
+    this.timestamp = ZERO_BD;
+    this.blockNumber = ZERO_BD;
+    this.mints = [];
+    this.burns = [];
+    this.swaps = [];
+  }
+
+  // function to convert database object class into the return object
+  // as defined in graphql schema
+  // create blockType from block
+  toGenerated(transactiondb: TransactionDb) {
+    this._id = transactiondb._id;
+    this.id = transactiondb.id;
+    this.timestamp = transactiondb.timestamp;
+    this.blockNumber = transactiondb.blockNumber;
+    this.mints = transactiondb.mints;
+    this.burns = transactiondb.burns;
+    this.swaps = transactiondb.swaps;
+  }
+}
