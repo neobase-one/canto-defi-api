d8f4e8a87f2df52d02e80d7eca51323ec2d9a61d
C M Sanjay Krishna <sanjay.krishna@mystertech.com>
2022-08-14 21:00:16 +0530

    update:transactions query optimisation

--- src/models/dex/transaction.ts
+++ src/models/dex/transaction.ts
@@ -80,9 +80,6 @@ export class Transaction {
     this.id = "";
     this.timestamp = ZERO_BD;
     this.blockNumber = ZERO_BD;
-    this.mints = [];
-    this.burns = [];
-    this.swaps = [];
   }
 
   async fromDb(txn: TransactionDb) {
@@ -90,50 +87,10 @@ export class Transaction {
     this.id = txn.id;
     this.timestamp = txn.timestamp;
     this.blockNumber = txn.blockNumber;
-
-    let mintTypes = [];
-    for (let mintId of txn.mints) {
-      var m = await this.getMint(mintId);
-      mintTypes.push(m);
-    }
-    this.mints = mintTypes;
-
-    let burnTypes = [];
-    for (let burnId of txn.burns) {
-      var b = await this.getBurn(burnId);
-      burnTypes.push(b);
-    }
-    this.burns = burnTypes;
-
-    let swapTypes = [];
-    for (let swapId of txn.swaps) {
-      var s = await this.getSwap(swapId);
-      swapTypes.push(s);
-    }
-    this.swaps = swapTypes;
-
     return this;
   }
 
   justId(id: string) {
     this.id = id;
-  }
-
-  async getMint(id: string): Promise<Mint> {
-    const mintDb = await MintModel.findOne({ id: id });
-    const mint = await mintDb?.toGenerated()
-    return mint as Mint;
-  }
-
-  async getSwap(id: string): Promise<Swap> {
-    const swapDb = await SwapModel.findOne({ id: id });
-    const swap = await swapDb?.toGenerated()
-    return swap as Swap;
-  }
-
-  async getBurn(id: string): Promise<Burn> {
-    const burnDb = await BurnModel.findOne({ id: id });
-    const burn = await burnDb?.toGenerated()
-    return burn as Burn;
   }
 }
